7d288f8567f13eb7c0e066ac134aece84b06749d
PADO <62028267+47PADO47@users.noreply.github.com>
2022-04-03 01:37:44 +0200

    🔊🥅✨
    🔊 - JS files logging to console
    🥅 - res.status != 200
    ✨ - Page input

--- pages/js/book.js
+++ pages/js/book.js
@@ -1,3 +1,5 @@
+console.log('%c [SUCCESS]', 'font-size: 1.5em; color: #00ff00;', '"book.js" file loaded');
+
 const repo = '47PADO47/solu.zone';
 const url = (name) => `https://api.github.com/repos/${repo}/contents/books/${name}.json`
 
@@ -5,8 +7,17 @@ async function fetchBook(name) {
     const al = document.getElementById('alert');
 
     const res = await fetch(url(name));
+
+    if (res.status !== 200) {
+        console.log('%c [ERROR] %c fetchBook() failed', 'font-size: 1.5em; color: #ff0000;', '');
+        al.setAttribute('hidden', false);
+        al.innerText = 'Errore di connessione';
+        return;
+    };
+    console.log('%c [INFO]', 'font-size: 1.5em; color: #800080;', 'fetch() returned');
+
     const data = await res.json().catch(err => {
-        console.log(err);
+        console.log('%c [ERROR] %c fetchBook() failed (non-json)', 'font-size: 1.5em; color: #ff0000;', '');
         al.setAttribute('hidden', false);
         al.innerText = 'Errore di connessione';
     });
@@ -14,16 +25,20 @@ async function fetchBook(name) {
 };
 
 async function updateHTML() {
-    const path = location.pathname.split('/').pop();
+    const path = location.pathname.split('/').pop().replace('.html', '');
     const book = await fetchBook(path);
 
     const content = atob(book.content);
     const json = JSON.parse(content);
-
     const { name, units, data } = json;
+
+    console.log('%c [INFO]', 'font-size: 1.5em; color: #800080;', 'book loaded');
 
     const title = document.getElementById('title');
     title.innerText = name;
+    title.style = 'text-align:center;';
+
+    console.log('%c [INFO]', 'font-size: 1.5em; color: #800080;', 'Updated title');
 
     const div = document.getElementById('container');
     data.forEach(element => {
@@ -41,6 +56,7 @@ async function updateHTML() {
                 if (i == 0) return;
                 const h3 = document.createElement('h3');
                 h3.innerText = "Pag. " + page[0];
+                h3.id = `page${page[0]}`;
                 div.appendChild(h3);
 
                 const exercises = document.createElement('div');
@@ -57,6 +73,7 @@ async function updateHTML() {
             data.forEach(page => {
                 const h3 = document.createElement('h3');
                 h3.innerText = "Pag. " + page[0];
+                h3.id = `page${page[0]}`;
                 div.appendChild(h3);
 
                 const exercises = document.createElement('div');
@@ -71,4 +88,11 @@ async function updateHTML() {
             });
         };
     });
+    return console.log('%c [SUCCESS]', 'font-size: 1.5em; color: #00ff00;', 'Updated content');
+};
+
+function goToPage() {
+    const input = document.getElementById('input');
+    const page = input.value;
+    window.location.href+=`#page${page}`;
 };
--- pages/js/books.js
+++ pages/js/books.js
@@ -1,20 +1,34 @@
+console.log('%c [SUCCESS]', 'font-size: 1.5em; color: #00ff00;', '"books.js" file loaded');
+
 const author = '47PADO47';
 const repoName = 'solu.zone';
 const repo = author + '/' + repoName;
 const path = 'books';
 
-window.onload = async () => {
+async function getBooks() {
+
+    console.log('%c [INFO]', 'font-size: 1.5em; color: #800080;', 'getBooks() called');
+
     const al = document.getElementById('alert');
     const booksDiv = document.getElementById('books');
     
     const res = await fetch(`https://api.github.com/repos/${repo}/contents/${path}`);
+    
+    if (res.status !== 200) {
+        console.log('%c [ERROR] %c getBooks() failed', 'font-size: 1.5em; color: #ff0000;', '');
+        al.setAttribute('hidden', false);
+        al.innerText = 'Errore di connessione';
+        return;
+    };
+    console.log('%c [INFO]', 'font-size: 1.5em; color: #800080;', 'fetch() returned');
+
     const data = await res.json().catch(err => {
-        console.log(err);
+        console.log('%c [ERROR] %c getBooks() failed (non-json)', 'font-size: 1.5em; color: #ff0000;', '');
         al.setAttribute('hidden', false);
         al.innerText = 'Errore di connessione';
     });
 
-    const books = data.filter(book => book.name.endsWith('.md'));
+    const books = data.filter(book => book.name.endsWith('.json'));
     books.forEach(book => {
         const card = document.createElement('div');
         card.classList.add('card');
@@ -25,18 +39,21 @@ window.onload = async () => {
 
         const cardTitle = document.createElement('h5');
         cardTitle.classList.add('card-title');
-        cardTitle.innerText = book.name.replace('.md', '');
+        cardTitle.innerText = book.name.replace('.json', '');
 
         const cardLink = document.createElement('a');
         cardLink.classList.add('btn');
         cardLink.classList.add('btn-primary');
         cardLink.innerHTML = 'Go <i class="fa-solid fa-arrow-right">';
-        cardLink.href = `https://${author}.github.io/${repoName}/${path}/${book.name}`;
+        cardLink.href = `https://${author}.github.io/${repoName}/pages/${book.name.replace('.json', '')}`;
 
         cardBody.appendChild(cardTitle);
         cardBody.appendChild(cardLink);
         card.appendChild(cardBody);
 
         booksDiv.appendChild(card);
+
+        return console.log('%c [INFO]', 'font-size: 1.5em; color: #800080;', `book ${book.name} added`);
     });
+    return console.log('%c [SUCCESS]', 'font-size: 1.5em; color: #00ff00;', 'Updated books!');
 };
