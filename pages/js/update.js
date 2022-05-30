80febfcfbd774a9fa4558423931311a023449ed4
codefactor-io <support@codefactor.io>
2022-05-30 15:08:40 +0000

    [CodeFactor] Apply fixes to commit 453ea35

--- pages/js/update.js
+++ pages/js/update.js
@@ -12,13 +12,13 @@ function checkInstallation() {
         alert.hidden = false;
         alert.innerText = 'ServiceWorker is not supported ❌';
         return hide();
-    };
+    }
     if (navigator.standalone) return hide();
     if (window.matchMedia('(display-mode: standalone)').matches) return hide();
     if (isIOS()) return prompt.style.display = 'none';
     else return instructions.style.display = 'none';
-};
+}
 
 function isIOS() {
     return navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPhone/i);
-};
+}
